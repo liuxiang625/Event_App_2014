118b84c80737fa897f21ef2a013b529b4221ac26
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-24 17:35:18 -0700

    Session Editing tab added

--- summitapp2013/WebFolder/DashBoard.waPage/scripts/index.js
+++ summitapp2013/WebFolder/DashBoard.waPage/scripts/index.js
@@ -2,11 +2,28 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button3 = {};	// @button
+	var button4 = {};	// @button
 	var button2 = {};	// @button
 	var dataGrid2 = {};	// @dataGrid
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		sources.session.save({
+	        onSuccess: function(event) {
+	                // displays success message in a DisplayError area
+	           $$('tabView2').selectTab(1);
+	        }
+    	});
+	};// @lock
+
+	button4.click = function button4_click (event)// @startlock
+	{// @endlock
+		$$('tabView2').selectTab(1);
+	};// @lock
 
 	button2.click = function button2_click (event)// @startlock
 	{// @endlock
@@ -19,6 +36,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button3", "click", button3.click, "WAF");
+	WAF.addListener("button4", "click", button4.click, "WAF");
 	WAF.addListener("button2", "click", button2.click, "WAF");
 	WAF.addListener("dataGrid2", "onRowDblClick", dataGrid2.onRowDblClick, "WAF");
 // @endregion
